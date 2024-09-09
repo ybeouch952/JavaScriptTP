06d61e54633c5e1758341ae34335a0813f58ed50
ybeouch952 <younesbeouch10@gmail.com>
2024-09-09 12:09:35 +0200

    Add files via upload

---
+++ Boucle/exoboucle.js
@@ -0,0 +1,53 @@
+// for (let k=1;k<11;k++)
+// {
+//     console.log(k);
+// }
+
+// let i=10
+// while (i>=0)
+// {
+//     console.log(i);
+//     i-=1
+// }
+
+// for (let k=0; k<21;k+=2)
+// {
+//     console.log(k)
+// }
+
+
+// function somme(n)
+// {
+//     let somme =0
+//     for (let i=1; i<=n; i++ )
+//     {
+//         somme +=i
+//     }
+//     console.log(somme)
+// }
+// somme(2)
+
+// function multiplicaiton(n)
+// {
+//     for ( let i=0; i<11; i++)
+//     {
+//         let multi = i*n
+//         console.log(multi)
+//     }
+// }
+// multiplicaiton(5)
+
+// monTableau = ["Javascript", "est", "excellent", "comme", "langage"]
+// for (element of monTableau ){
+//     console.log(element)
+// }
+
+// MonObjet = {Prenom : "Younes", Nom : "Beouch", age : "20 ans"}
+// for (element in MonObjet){
+//     console.log(element+ " : "+MonObjet[element])
+// }
+
+// let tab = [0, 22, 32];
+// tab.forEach(function(element){
+//     console.log(element**2)
+// })
