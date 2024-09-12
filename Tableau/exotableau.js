68a46a5f0b39b64b3437f156274fc2f64f6af83c
ybeouch952 <younesbeouch10@gmail.com>
2024-09-12 13:11:08 +0200

    Add files via upload

---
+++ Tableau/exotableau.js
@@ -0,0 +1,40 @@
+tab = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(tab[0])
+// console.log(tab[9])
+
+// tab[9] = 0
+
+// tab.push(11,12)
+// tab.shift(tab[0])
+
+// function sommetab (tab)
+// {
+//     let somme = 0
+//     for (let i in tab) {
+//         somme += tab[i]
+//     }
+//     return somme 
+// }
+// console.log(sommetab(tab))
+
+// function recherchetab(tab,val)
+// {
+//     for ( let i in tab)
+//     {
+//         if (val === tab[i])
+//             {
+//             return true;
+//         }
+//     }
+//         return false
+// }
+// console.log(recherchetab(tab,50))
+
+// tab2 = [20,22,24,26,28]
+// function fusiontableau(tab,tab2)
+// {
+//     let tab3 = tab.concat(tab2)
+//     return tab3
+// }
+// console.log(fusiontableau(tab,tab2))
