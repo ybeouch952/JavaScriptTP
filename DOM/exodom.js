d644813200d5c94b13341159aee9f651c2e1bb62
ybeouch952 <younesbeouch10@gmail.com>
2024-09-19 17:17:38 +0200

    Add files via upload

---
+++ DOM/exodom.js
@@ -0,0 +1,21 @@
+// let p = document.getElementById('monParagraphe');
+// p.innerHTML = "Ceci est un nouveau texte pour le paragraphe";
+
+
+// // let p = document.getElementById('monParagraphe');
+// // p.style.display = 'none'; // 
+// let elementASupprimer = document.getElementById('monParagraphe')
+// elementASupprimer.remove()
+
+
+// let liste = document.getElementById('maListe');
+// let  elements = ["You", "Ne", "s"];
+// elements.forEach(function(item) {
+//     const newElement = document.createElement('li');
+//     newElement.textContent = item;
+//     liste.appendChild(newElement);
+// });
+
+let liste = document.getElementById('maListe')
+let items = liste.getElementsByTagName('li')
+liste.removeChild(items[1])
