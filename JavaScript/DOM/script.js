4a142b2ba6b5b26cf84dcbf715b2ca21874932eb
ybeouch952 <younesbeouch10@gmail.com>
2024-10-10 17:14:23 +0200

    Delete JavaScript directory

--- JavaScript/DOM/script.js
+++
@@ -1,7 +0,0 @@
-// let h1 = document.getElementById("titre") 
-// console.log(h1)
-
-let elements = document.getElementsByClassName("paragraphe");
-    for (let i = 0; i < elements.length; i++) {
-        console.log(elements[i]);  
-    }
